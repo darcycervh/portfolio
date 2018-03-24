9f712fca6c3a22b550752b7dc9de007a4c04e044
darcycervh <darcycervera@gmail.com>
2018-03-24 03:44:19 -0500

    añadiendo avance

--- js/about.js
+++ js/about.js
@@ -1,17 +1,16 @@
 $(document).ready(function(){
-  var $spanStarPortfolio = $('#span-start-portfolio');
   // vistas del portafolio
   var $about = $('#about');
   var $portfolio = $('#portfolio');
   var $contact = $('#contact');
   // redireccionando a las siguientes vistas
   $('#about').on('click', function() {
-    window.location.href = '../portfolio/views/about.html';
+    window.location.href = '../views/about.html';
   });
   $portfolio.on('click', function() {
-    window.location.href = '../portfolio/views/portfolio.html';
+    window.location.href = '../views/portfolio.html';
   });
   $contact.on('click', function() {
-    window.location.href = '../portfolio/views/contact.html';
+    window.location.href = '../views/contact.html';
   });
 })
