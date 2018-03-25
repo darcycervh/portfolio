3897b1ae4980b80f49f0f47c7ebd23d54a987670
darcycervh <darcycervera@gmail.com>
2018-03-25 17:20:55 -0500

    añadiendo proyectos

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,12 +1,5 @@
 $(document).ready(function(){
-  var $more = $('.more');
-  var $back = $('.back');
   $('.more').click(function(){
     $('.back').addClass('active');
-    $('.front').removeClass('active');
-  })
-  $('.go-back').click(function(){
-    $('.back').removeClass('active');
-    $('.front').addClass('active');
   })
 })
